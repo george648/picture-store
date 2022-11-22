0f8e36cfbee74446311006fb6e320daf9d8d4303
george.balashevich@gmail.com <35461002238898@mail.ru>
2022-11-22 23:28:15 +0300

    handle auto open modal and added animation

--- src/js/module/modals.js
+++ src/js/module/modals.js
@@ -1,5 +1,6 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+  let btnPressed = false
+  function bindModal(triggerSelector, modalSelector, closeSelector, destroy = false ) {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
@@ -12,8 +13,15 @@ const modals = () => {
           e.preventDefault()
         }
 
+        btnPressed = true
+
+        if(destroy) {
+          item.remove()
+        }
+
         windows.forEach(item => {
           item.style.display = 'none'
+          item.classList.add('animated', 'fadeIn')
         })
 
         modal.style.display = "block"
@@ -33,7 +41,7 @@ const modals = () => {
     })
 
     modal.addEventListener('click', (e) => {
-      if (e.target === modal && closeClickOverlay) {
+      if (e.target === modal) {
         windows.forEach(item => {
           item.style.display = 'none'
         });
@@ -48,7 +56,7 @@ const modals = () => {
   function showModalByTime(selector, time) {
     setTimeout(function () {
       let display
-      
+
       document.querySelectorAll('[data-modal]').forEach(modal => {
         if (getComputedStyle(modal).display !== 'none') {
           display = 'block'
@@ -56,8 +64,10 @@ const modals = () => {
       })
 
       if (!display) {
+        let scroll = calcScroll()
         document.querySelector(selector).style.display = 'block'
         document.body.style.overflow = "hidden"
+        document.body.style.marginRight = `${scroll}px`
       }
 
     }, time)
@@ -78,9 +88,19 @@ const modals = () => {
     return scrollWidth
   }
 
+  function openByScroll(selector) {
+    window.addEventListener('scroll', () => {
+      if(!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight)) {
+        document.querySelector(selector).click()
+      }
+    })
+  }
+
+  bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true)
+  openByScroll('.fixed-gift')
   bindModal('.button-design', '.popup-design', '.popup-design .popup-close')
   bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close')
-  showModalByTime('.popup-consultation', 6000)
+  // showModalByTime('.popup-consultation', 6000)
 }
 
 export default modals
